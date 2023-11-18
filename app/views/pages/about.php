8e7af5cabf61ac5e0679f33acd09cede409c8200
PythonScript1 <loussalmohammeddev@gmail.com>
2023-11-18 13:49:22 +0100

    pages index and about pages

---
+++ app/views/pages/about.php
@@ -0,0 +1,5 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+  <h1>About</h1>
+  <p>This is an Online Market Place Where you can sell your product and buy product.</p>
+  <p>App Version: <?php echo $data['version']; ?></p>
+<?php require APPROOT . '/views/inc/footer.php'; ?>
