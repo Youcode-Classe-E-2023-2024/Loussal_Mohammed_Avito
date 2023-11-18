ce77ecd1a80c72e90831d624c1a4a50ee1314ad5
PythonScript1 <loussalmohammeddev@gmail.com>
2023-11-18 12:49:18 +0100

    creating config.php which defines DB params and App root + Url root + Site Name

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+  // DB Params
+  define("DB_HOST", "localhost");
+  define("DB_USER", "root");
+  define("DB_PASS", "112358");
+  define("DB_NAME", "LoussalMohammedAvito");
+
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'http://localhost/project');
+  // Site Name
+  define('SITENAME', 'LoussalMohammedAvito');
+  
+?>
